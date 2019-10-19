a19d58c22c68b7f6a7575c0d006219cdb29db0e7
bastien girschig <bastien.girschig@gmail.com>
2019-10-19 17:42:30 +0200

    update index

--- src/index.js
+++ src/index.js
@@ -2,7 +2,28 @@
  * @package
  */
 
-import * as promises from './promises';
 import * as array from './array';
+import * as dom from './dom';
+import * as math from './math';
+import * as promises from './promises';
+import NumpyLoader from './NumpyLoader';
+import Quadtree from './Quadtree';
+import Rectangle from './Rectangle';
+import StateMachine from './StateMachine';
+import Vector3 from './Vector3';
+import WorkerHandler from './WorkerHandler';
+import WorkerWrapper from './WorkerWrapper';
 
-export {promises, array};
+export {
+  array,
+  dom,
+  math,
+  promises,
+  NumpyLoader,
+  Quadtree,
+  Rectangle,
+  StateMachine,
+  Vector3,
+  WorkerHandler,
+  WorkerWrapper,
+};
